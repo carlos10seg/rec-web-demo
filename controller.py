9d5843fbe04b4cd373272001fd5c9ae0c797ce79
Carlos <carlos@carloss-mbp-2.boisestate.edu>
2020-09-05 14:15:48 -0600

    update UI and controller

--- controller.py
+++ controller.py
@@ -8,16 +8,17 @@ class Controller:
     def get_recs_from_recserver(self, users, nr_recs, algo, items):
         config_reader = ConfigReader()
         base_url = config_reader.get_value("rec_server_url")
-        is_a_rec_request = True if algo == 'popular' or algo == 'topn' else False
+        #is_a_rec_request = True if algo == 'popular' or algo == 'topn' else False
         recs = []
         for userId in users:
-            if is_a_rec_request:
-                url = f'{base_url}/algorithms/{algo}/recommendations?user_id={userId}&num_recs={nr_recs}'
-            else:
-                url = f'{base_url}/algorithms/{algo}/predictions?user_id={userId}&items={items}'
+           # if is_a_rec_request:
+            url = f'{base_url}/algorithms/{algo}/recommendations?user_id={userId}&num_recs={nr_recs}'
+            # else:
+            #     url = f'{base_url}/algorithms/{algo}/predictions?user_id={userId}&items={items}'
             r = requests.get(url)
             data = r.json()
-            recs.append({'user': userId, 'recs': data['recommendations'] if is_a_rec_request else data['predictions']})
+            #recs.append({'user': userId, 'recs': data['recommendations'] if is_a_rec_request else data['predictions']})
+            recs.append({'user': userId, 'recs': data['recommendations']})
         return recs
         
         # print(data)
@@ -48,6 +49,11 @@ class Controller:
         for current_recs in all_recs:
             for rec in current_recs['recs']:
                 rec['title'] = dbManager.get_movie(rec['item']).loc[0, 'title']
+                # movie_name = ui.item.value.substr(0, ui.item.value.length - 5)
+                # movie_year = ui.item.value.substr(ui.item.value.length - 5).replace(')', '')
+                movie_name = rec['title'][:-5]
+                movie_year = rec['title'][-5:].replace(')', '')
+                rec['image_url'] = self.get_image_url(movie_name, movie_year)
 
         return all_recs
 
